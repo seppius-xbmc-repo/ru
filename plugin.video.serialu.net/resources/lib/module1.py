5cd4b835f5fa7a0154d05449e905bb63efd9d790
xbmcrepo <xbmc@inbox.ru>
2015-03-21 13:43:40 +0300

    add plugin.video.serialu.net-1.2.6

---
+++ plugin.video.serialu.net/resources/lib/module1.py
@@ -0,0 +1,25 @@
+﻿import urllib2, urllib, re, cookielib, sys, time
+
+# load XML library
+sys.path.append(r'g:\XBMC\resources\lib')
+from ElementTree  import Element, SubElement, ElementTree
+
+window = Element("window")
+
+title = SubElement(window, "title", font="large")
+title.text = ("Проверка АБВГД..").decode('utf-8')
+
+text = SubElement(window, "text", wrap="word")
+
+box = SubElement(text, "buttonbox1")
+SubElement(box, "button").text = ("OK").decode('utf-8')
+SubElement(box, "button").text = ("Проба записи").decode('utf-8')
+
+box = SubElement(text, "buttonbox2")
+SubElement(box, "button").text = ("Error").decode('utf-8')
+SubElement(box, "button").text = ("Але! Гараж").decode('utf-8')
+
+ElementTree(window).write(r'g:\xbmc\resources\data\test.xml', encoding='utf-8')
+
+
+
---
+++ plugin.video.serialu.net/resources/lib/module2.py
@@ -0,0 +1,16 @@
+﻿import urllib2, urllib, re, cookielib, sys, time
+
+# load XML library
+sys.path.append(r'g:\XBMC\resources\lib')
+from ElementTree  import Element, SubElement, ElementTree
+
+tree = ElementTree()
+tree.parse(r'g:\xbmc\resources\data\test.xml')
+
+for i in tree.getroot():             # Iterates through all found links
+    print i.text
+    print i.tag
+
+for k in tree.find('text').find('buttonbox2'):
+    print k.text
+#tree.write("output.xhtml")
