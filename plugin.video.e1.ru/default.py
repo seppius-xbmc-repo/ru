56f08c53d3becca8f734c3b6d3f56d8546b60416
xbmcrepo <xbmc@inbox.ru>
2015-03-21 11:52:23 +0300

    add plugin.video.e1.ru-0.0.1

---
+++ plugin.video.e1.ru/default.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#   Copyright (c) 2011 XBMC-Russia, HD-lab Team, E-mail: dev@hd-lab.ru
+#   Writer (c) 25/05/2011, Kostynoy S.A., E-mail: seppius2@gmail.com
+#
+#   This Program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2, or (at your option)
+#   any later version.
+#
+#   This Program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; see the file COPYING.  If not, write to
+#   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#   http://www.gnu.org/licenses/gpl.html
+
+import sys, os
+
+sys.path.append(os.path.join(os.getcwd().replace(';', ''), 'resources', 'lib'))
+
+if (__name__ == "__main__" ):
+	import addon
+	addon.addon_main()
