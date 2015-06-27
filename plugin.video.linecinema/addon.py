7fa0c2479789d0c999577a5517e1e684b6d601d1
xbmcrepo <xbmc@inbox.ru>
2015-06-27 19:12:55 +0300

    add plugin.video.linecinema_0.0.2

--- plugin.video.linecinema/addon.py
+++ plugin.video.linecinema/addon.py
@@ -3,46 +3,72 @@
 import urllib, urllib2, re, sys
 import xbmcplugin, xbmcgui
 
-def getHTML(url):
+def getHTML(url, data = False):
     headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3', 'Content-Type':'application/x-www-form-urlencoded'}
-    conn = urllib2.urlopen(urllib2.Request(url, urllib.urlencode({}), headers))
+    if data == False:
+        conn = urllib2.urlopen(urllib2.Request(url, urllib.urlencode({}), headers))
+    else:
+        conn = urllib2.urlopen(urllib2.Request(url, urllib.urlencode(data), headers))
     
     html = conn.read()
     conn.close()
     
     return html
 
+def showkeyboard(txtMessage="",txtHeader="",passwordField=False):
+    if txtMessage=='None': txtMessage=''
+    keyboard = xbmc.Keyboard(txtMessage, txtHeader, passwordField)#("text to show","header text", True="password field"/False="show text")
+    keyboard.doModal()
+    if keyboard.isConfirmed():
+        return keyboard.getText()
+    else:
+        return False # return ''
+
+def Search():
+    text = showkeyboard('', 'Поиск по названию')
+    url = 'http://linecinema.org/'
+    data =  {'do': 'search', 'subaction': 'search', 'story' : text.decode('utf-8').encode('windows-1251')}
+    html = getHTML(url, data)
+    movie_links = re.compile('<h1>\s*<a href="(.+?)">(.+?)<\/a>\s*<\/h1>').findall(html.decode('windows-1251').encode('utf-8'))
+    pictures_link = re.compile('<img\ssrc="http\:\/\/www.linecinema.org([^"]+)"\swidth="180">').findall(html.decode('windows-1251').encode('utf-8'))
+    for i in range(0, len(movie_links)):
+        if len(pictures_link) > i:
+            addDir(movie_links[i][1], movie_links[i][0], 30, pictures_link[i])
+        else:
+            addDir(movie_links[i][1], movie_links[i][0], 30, None)
+
 def isLinkUseful(needle):
     haystack = ['/index.php', '/newsz/Televydenye/100432-2008-3-11-432.html', '/newsz/500183-tex-podderzhka.html']
-    for a in haystack:
-        if needle == a:
-            return False
-
-    return True
+    return needle not in haystack
 
 def Categories():
-	url = 'http://www.linecinema.org'
-	html = getHTML(url)
-	genre_links = re.compile('<a href="(.+?)" title="" class="mainmenu">(.+?)</a><br />').findall(html.decode('windows-1251').encode('utf-8'))
+    url = 'http://www.linecinema.org'
+    html = getHTML(url)
+    genre_links = re.compile('<a href="(.+?)" title="" class="mainmenu">(.+?)</a><br />').findall(html.decode('windows-1251').encode('utf-8'))
+    addDir('Поиск...', '', 35, None)
+    for link, title in genre_links:
+        if isLinkUseful(link):
+            addDir(title, url + link, 20, None)
 
-	for link, title in genre_links:
-	    if isLinkUseful(link):
-	        addDir(title, url + link, 20)
+def Movies(url, page=1):
+    html = getHTML(url + 'page/' + str(page) + '/')
+    movie_links = re.compile('<h1> <a href="(.+?)">(.+?)</a>   </h1>').findall(html.decode('windows-1251').encode('utf-8'))
+    pictures_link = re.compile('<img src="(.+?)" width="180"></a>').findall(html.decode('windows-1251').encode('utf-8'))
+    #next_link = re.compile('<a href="(.+?)">.+?</a></div></div>').findall(html.decode('windows-1251').encode('utf-8'))
 
-def Movies(url):
-	html = getHTML(url)
-	movie_links = re.compile('<h1> <a href="(.+?)">(.+?)</a>   </h1>').findall(html.decode('windows-1251').encode('utf-8'))
+    addDir("<< На главную", url, None, None)
 
-	for link, title in movie_links:
-	    addDir(title[:-12], link, 30)
+    for i in range(0, len(movie_links)):
+        addDir(movie_links[i][1], movie_links[i][0], 30, pictures_link[i])
 
-def Videos(url, title):
-	html = getHTML(url)
-	link = re.compile('file:   "(.+?)"').findall(html.decode('windows-1251').encode('utf-8'))[0]
+    if len(movie_links) >= 15:
+        addDir("Следующая страница >>", url , 20, None, str(int(page) + 1))
 
-	addLink(title, link)
+def Videos(url, title, picture):
+    html = getHTML(url)
+    link = re.compile('file:   "(.+?)"').findall(html.decode('windows-1251').encode('utf-8'))[0]
 
-	    
+    addLink(title, link, picture)
 
 
 def get_params():
@@ -64,43 +90,65 @@ def get_params():
     return param
 
 
-def addLink(title, url):
-    item = xbmcgui.ListItem(title, iconImage='DefaultVideo.png', thumbnailImage='')
+def addLink(title, url, picture):
+    if  picture == None:
+        item = xbmcgui.ListItem(title, iconImage='DefaultVideo.png', thumbnailImage='')
+    else:
+        item = xbmcgui.ListItem(title, iconImage='DefaultVideo.png', thumbnailImage='http://www.linecinema.org/' + picture)
     item.setInfo( type='Video', infoLabels={'Title': title} )
 
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=item)
 
 
-def addDir(title, url, mode):
+def addDir(title, url, mode, picture, page=None):
     sys_url = sys.argv[0] + '?title=' + urllib.quote_plus(title) + '&url=' + urllib.quote_plus(url) + '&mode=' + urllib.quote_plus(str(mode))
-
-    item = xbmcgui.ListItem(title, iconImage='DefaultFolder.png', thumbnailImage='')
-    item.setInfo( type='Video', infoLabels={'Title': title} )
+    if  picture == None:
+        item = xbmcgui.ListItem(title, iconImage='DefaultFolder.png', thumbnailImage='')
+    else:
+        item = xbmcgui.ListItem(title, iconImage='DefaultFolder.png' , thumbnailImage='http://www.linecinema.org' + picture)    
+        sys_url += '&picture=' + urllib.quote_plus(str(picture))
+    if page != None:
+        sys_url += '&page=' + urllib.quote_plus(str(page))
+    item.setInfo(type='Video', infoLabels={'Title': title})
 
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=sys_url, listitem=item, isFolder=True)
 
 
 params = get_params()
-url    = None
-title  = None
-mode   = None
+url = None
+title = None
+mode = None
+picture = None
+page = 1
 
-try:    title = urllib.unquote_plus(params['title'])
-except: pass
-
-try:    url = urllib.unquote_plus(params['url'])
-except: pass
-
-try:    mode = int(params['mode'])
-except: pass
+try:
+    title = urllib.unquote_plus(params['title'])
+except:
+    pass
+try:
+    url = urllib.unquote_plus(params['url'])
+except:
+    pass
+try:
+    mode = int(params['mode'])
+except:
+    pass
+try:
+    picture = params['picture']
+except:
+    pass
+try:
+    page = int(params['page'])
+except:
+    pass
 
 if mode == None:
     Categories()
-
 elif mode == 20:
-    Movies(url)
-
+    Movies(url, page)
 elif mode == 30:
-    Videos(url, title)
+    Videos(url, title, picture)
+elif mode == 35:
+    Search()
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
