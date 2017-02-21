ff0daac06c46b31c8490325279d21a783f0431f1
akgdrg@googlemail.com <akgdrg@googlemail.com>
2017-02-21 22:59:52 +0100

    shura addon extended support for kodi 17

--- plugin.video.shura.tv/ThreeMinBack.py
+++ plugin.video.shura.tv/ThreeMinBack.py
@@ -5,23 +5,22 @@ import string, xbmc, xbmcgui, xbmcplugin, os, xbmcaddon
 addon = xbmcaddon.Addon(id='plugin.video.shura.tv')
 step = addon.getSetting('BigStepBackward')
 
-myPlayer=xbmc.PLAYER_CORE_AUTO
 try:
 	item=xbmcgui.ListItem('', '', '', '')
-	archiveName=str(xbmc.Player(myPlayer).getVideoInfoTag().getTitle())
+	archiveName=str(xbmc.Player().getVideoInfoTag().getTitle())
 	#xbmc.log('Name0='+archiveName)
-	archiveurl=xbmc.Player(myPlayer).getPlayingFile().split('=')[0]
+	archiveurl=xbmc.Player().getPlayingFile().split('=')[0]
 	#xbmc.log('url='+archiveurl)
-	archivetime=xbmc.Player(myPlayer).getPlayingFile().split('=')[1]
+	archivetime=xbmc.Player().getPlayingFile().split('=')[1]
 	#xbmc.log('time1='+str(archivetime))
-	playedtime=xbmc.Player(myPlayer).getTime()
+	playedtime=xbmc.Player().getTime()
 	#xbmc.log('played time='+str(playedtime)+',step='+str(step*60))
 	archivetime=int(archivetime)+int(playedtime)-int(step)*60
 	#xbmc.log('time2='+str(archivetime))
 	item.setInfo(type="Video", infoLabels={"Title": archiveName})
 	url=archiveurl+'='+str(archivetime)
 	#xbmc.log('url2='+url)
-	xbmc.Player(myPlayer).play(url,listitem=item)
+	xbmc.Player().play(url,listitem=item)
 except:
 	xbmc.log('[SHURA.TV] exception in OneMinBackward.py use default function for this key')
 	xbmc.executebuiltin('PlayerControl(BigSkipBackward)')
--- plugin.video.shura.tv/pagedownfix.py
+++ plugin.video.shura.tv/pagedownfix.py
@@ -2,9 +2,8 @@
 #/*
 import string, xbmc, xbmcgui, xbmcplugin, os, xbmcaddon
 
-myPlayer=xbmc.PLAYER_CORE_AUTO
-if 'tvshka' in xbmc.Player(myPlayer).getPlayingFile():
-	xbmc.Player(myPlayer).playprevious()
+if 'tvshka' in xbmc.Player().getPlayingFile():
+	xbmc.Player().playprevious()
 	xbmc.log('[PageDownFixExceputed] in SHURA.TV')
 else:
 	xbmc.executebuiltin('PlayerControl(Previous)')
