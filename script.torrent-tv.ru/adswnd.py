c8045c0d563d60f7215dca138011986c95f4bd11
xbmcrepo <xbmc@inbox.ru>
2017-03-16 19:55:40 +0300

    updated script.torrent-tv.ru 1.7.0

--- script.torrent-tv.ru/adswnd.py
+++
@@ -1,8 +0,0 @@
-﻿# Copyright (c) 2010-2011 Torrent-TV.RU
-# Writer (c) 2011, Welicobratov K.A., E-mail: 07pov23@gmail.com
-import xbmcgui
-import xbmc
-
-class AdsForm(xbmcgui.WindowXMLDialog):
-    def __init__(self, *args, **kwargs):
-        self.playing = False
