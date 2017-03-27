34a6b19159c88c2ca6dd2368ba0dca12d89b842a
dandygithub <denis.bolotin.dandy@gmail.com>
2017-03-27 12:37:41 +0300

    repair tushkan addon

--- plugin.video.tushkan.net/default.py
+++ plugin.video.tushkan.net/default.py
@@ -31,24 +31,38 @@ headers  = {
     'Accept-Language':'ru-RU,ru;q=0.9,en;q=0.8',
     'Accept-Charset' :'utf-8, utf-16, *;q=0.1',
     'Accept-Encoding':'identity, *;q=0'}
-    
 
+headers2  = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Connection':'keep-alive',
+    'Host':'tushkan.club',
+    'Referer':'http://tushkan.club/',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
-def GET(target, post=None):
+def GET(target, post=None, method='post'):
     target=target.replace('//page','/page')
     #print target
     try:
         cookiejar = cookielib.MozillaCookieJar()
-        urlOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar),antizapret.AntizapretProxyHandler())
+        if Addon.getSetting('antizapret') == 'true':
+            import antizapret
+            urlOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar),antizapret.AntizapretProxyHandler())
+        else: 
+            urlOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
         urllib2.install_opener(urlOpener)
-        #urlOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
-        request = urllib2.Request(url = target, data = post, headers = headers)
+        if method == 'post':
+            request = urllib2.Request(url = target, data = post, headers = headers)
+        else:
+            request = urllib2.Request(url = target, data = post, headers = headers2)
+            request.get_method = lambda: 'GET'
         url = urlOpener.open(request)
         http=url.read()
         return http
     except Exception, e:
-            xbmc.log( '[%s]: GET EXCEPT [%s]' % (addon_id, e), 4 )
-            showMessage('HTTP ERROR', e, 5000)
+        xbmc.log( '[%s]: GET EXCEPT [%s]' % (addon_id, e), 4 )
+        showMessage('HTTP ERROR', e, 5000)
 
 def construct_request(params):
     return '%s?%s' % (sys.argv[0], urllib.urlencode(params))
@@ -64,29 +78,37 @@ def showMessage(heading, message, times = 3000, pics = addon_icon):
 def doSearch(params):
     try: 
         lnk=params['link']
-        if 'tushkan.tv' not in lnk: lnk='http://tushkan.tv'+lnk
+        if 'tushkan.club' not in lnk: lnk='http://tushkan.club'+lnk
         http = GET(lnk)
     except: 
-        lnk='http://tushkan.tv/'
+        lnk='http://tushkan.club/'
         http = GET(lnk)
 
     #print http
     #beautifulSoup = BeautifulSoup(http)
-    names=re.compile('е:<\/b>([^#<]+)<br').findall(http)
+    names=re.compile('Оригинальное название:<\/b>([^#<]+)<br').findall(http)
+    descriptions=re.compile('Описание:<\/b>([^#<]+)<span').findall(http)
     #print names
-    lnks=re.compile('<a\s*href="([^#<]+)"\s*title="([^"<]+)"[^>]+>\s*<img.*src="([^#<]+)"[^О]*<hr><b>Оригинальное название:<\/b>([^#<]+)<').findall(http)
-    i=0
+    lnks=re.compile('<a\s*href="([^#<]+)"\s*title="([^"<]+)"[^>]+>\s*<img.*src="([^#<]+)"[^О]*><b>Оригинальное название:<\/b>([^#<]+)<').findall(http)
+
+    i=0                                                                                   
     #print len(names), len(lnks)
     for ch in lnks:
         title=ch[3].strip()
+        description = title
+        try:
+            description = descriptions[i].strip() + '...' if descriptions[i] else title
+        except:
+            pass
         url=ch[0]
-        if 'tushkan.tv' in ch[2]: img=ch[2]
-        else:        img= 'http://tushkan.tv'+ch[2]
-        li = xbmcgui.ListItem(title, iconImage = addon_icon, thumbnailImage = img)
+        if 'tushkan.club' in ch[2]: img=ch[2]
+        else:        img= 'http://tushkan.club'+ch[2]
+        li = xbmcgui.ListItem(title, iconImage = img, thumbnailImage = img)
         uri = construct_request({
                         'func': 'openitem',
                         'mpath': url
         })
+        li.setInfo(type='Video', infoLabels={'title': title, 'plot': description})
         li.setProperty('fanart_image', addon_fanart)
         xbmcplugin.addDirectoryItem(hos, uri, li, True)
         i=i+1
@@ -102,11 +124,12 @@ def doSearch(params):
         li.setProperty('fanart_image', addon_fanart)
         xbmcplugin.addDirectoryItem(hos, uri, li, True)
     except: pass
+    xbmcplugin.setContent(hos, 'movies')
     xbmcplugin.endOfDirectory(hos)
 
 def openitem(params):
-    lk="http://tushkan.tv"+params['mpath']
-    http = GET(lk)
+    lk="http://tushkan.club"+params['mpath']
+    http = GET(lk, method = 'get')
     #print lk
 
     #print titl.encode('utf-8')
@@ -115,14 +138,15 @@ def openitem(params):
 
     #ttt=re.compile('title="([^"]+)"\s*src="([^"]+)"><\/div>').findall(http)
     ttt=re.compile('title="([^"]+)"\s*src="([^"]+)">').findall(http)
-    img="http://tushkan.tv"+ttt[0][1]
+    img="http://tushkan.club"+ttt[0][1]
     ttt2=ttt[0][0]
     hh= re.compile('http://.+film/.+.flv')
     tttl= (re.findall(hh,str(http)))
     #img=addon_icon
     if tttl:
         link=tttl[0]
-        li = xbmcgui.ListItem(ttt2, iconImage = addon_icon, thumbnailImage = img)
+        li = xbmcgui.ListItem(ttt2, iconImage = img, thumbnailImage = img)
+        li.setInfo(type='Video', infoLabels={'title': ttt2, 'plot': ttt2})
         uri = construct_request({
             'func': 'playitem',
             'mpath': link
@@ -131,9 +155,9 @@ def openitem(params):
         li.setProperty('IsPlayable', 'true')
         xbmcplugin.addDirectoryItem(hos, uri, li)
     else:
-        hh= re.compile('http://.+.txt')
+        hh = re.compile('"http://.+.txt')
         tttl= (re.findall(hh,str(http)))
-        link=tttl[0]
+        link=tttl[1].replace('"', '')
         http = GET(link)
         #print http
         link=re.findall('pl :"(.+)"',str(http))[0].replace("'",'"')
@@ -149,24 +173,27 @@ def openitem(params):
                     li = xbmcgui.ListItem(it['comment'].replace('<b>','[COLOR FF0FF000]').replace('</b>','[/COLOR]'), iconImage = addon_icon, thumbnailImage = img)
                     xbmcplugin.addDirectoryItem(hos, '', li)
                     for itt in pl0:
-                        li = xbmcgui.ListItem(itt['comment'], iconImage = addon_icon, thumbnailImage = img)
+                        li = xbmcgui.ListItem(itt['comment'], iconImage = img, thumbnailImage = img)
                         uri = construct_request({
                             'func': 'payser',
-                            'mpath': itt['file']
+                            'mpath': itt['file'].replace('.xyz', '.net')
                         })
+                        li.setInfo(type='Video', infoLabels={'title': itt['comment'], 'plot': itt['comment']})
                         li.setProperty('fanart_image', addon_fanart)
                         li.setProperty('IsPlayable', 'true')
                         xbmcplugin.addDirectoryItem(hos, uri, li)
                 else:
-                    li = xbmcgui.ListItem(it['comment'], iconImage = addon_icon, thumbnailImage = img)
+                    li = xbmcgui.ListItem(it['comment'], iconImage = img, thumbnailImage = img)
                     uri = construct_request({
                         'func': 'payser',
                         'mpath': it['file']
                     })
+                    li.setInfo(type='Video', infoLabels={'title': it['comment'], 'plot': it['comment']})
                     li.setProperty('fanart_image', addon_fanart)
                     li.setProperty('IsPlayable', 'true')
                     xbmcplugin.addDirectoryItem(hos, uri, li)
 
+    xbmcplugin.setContent(hos, 'movies')
     xbmcplugin.endOfDirectory(hos)
     
 def playitem(params):
@@ -189,7 +216,7 @@ def _doSearch(params):
     if usearch:
         keyword = params['keyword']
         params={}
-        params['link']='http://tushkan.tv/search/?q='+urllib.quote(keyword)+'&sfSbm=%D0%98%D1%81%D0%BA%D0%B0%D1%82%D1%8C&a=45'
+        params['link']='http://tushkan.club/search/?q='+urllib.quote(keyword)+'&sfSbm=%D0%98%D1%81%D0%BA%D0%B0%D1%82%D1%8C&a=45'
         params['mode']='link'
         doSearch(params)
         return
@@ -201,7 +228,7 @@ def _doSearch(params):
     if kbd.isConfirmed():
         sts=kbd.getText()
         params={}
-        params['link']='http://tushkan.tv/search/?q='+urllib.quote(sts)+'&sfSbm=%D0%98%D1%81%D0%BA%D0%B0%D1%82%D1%8C&a=45'
+        params['link']='http://tushkan.club/search/?q='+urllib.quote(sts)+'&sfSbm=%D0%98%D1%81%D0%BA%D0%B0%D1%82%D1%8C&a=45'
         params['mode']='link'
         doSearch(params)
  
@@ -226,13 +253,14 @@ def mainScreen(params):
     li.setProperty('fanart_image', addon_fanart)
     xbmcplugin.addDirectoryItem(hos, uri, li, True)
     
-    http = GET('http://tushkan.tv/')
+    http = GET('http://tushkan.club/')
     #print http
     gogr=re.compile('"catName"\s*href="([^>]+)">([^"]+)<\/a>').findall(http)
     for nn in gogr:
         link= nn[0]
         tt= nn[1]
         li = xbmcgui.ListItem(tt, iconImage = addon_icon, thumbnailImage = addon_icon)
+        li.setInfo(type='Video', infoLabels={'title': tt, 'plot': tt})
         uri = construct_request({
             'func': 'doSearch',
             'link':link
@@ -240,7 +268,7 @@ def mainScreen(params):
         li.setProperty('fanart_image', addon_fanart)
         xbmcplugin.addDirectoryItem(hos, uri, li, True)
     
-    
+    xbmcplugin.setContent(hos, 'files')    
     xbmcplugin.endOfDirectory(handle=hos, succeeded=True, updateListing=False, cacheToDisc=True)
     
 def get_params(paramstring):
